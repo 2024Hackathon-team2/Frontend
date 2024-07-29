47690c97465805eacd9ac14a16875410539dcfef
yeeeww <yeangsshi@ewhain.net>
2024-07-30 00:01:01 +0900

    merge

--- Frontend/hackathon/src/pages/HomePage.js
+++ Frontend/hackathon/src/pages/HomePage.js
@@ -50,7 +50,7 @@ const Container = styled.div`
   background-color: white;
 `;
 
-const Header = styled.div`
+const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
