a601c77379c667dbcd070a3271a6b723b482e053
3olly <cy.kim@ewhain.net>
2024-08-01 18:48:57 +0900

    Fix:프론트 상 콘솔 에러 해결

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -57,8 +57,8 @@ const HomePage = () => {
                   rx="2.31579"
                   transform="rotate(-108.186 22.5158 27.0117)"
                   stroke="#C9C9C9"
-                  stroke-width="2.31579"
-                  stroke-linejoin="round"
+                  strokeWidth="2.31579"
+                  strokeLinejoin="round"
                 />
                 <rect
                   x="8.49683"
@@ -106,21 +106,21 @@ const HomePage = () => {
                   fill="url(#paint4_linear_767_1512)"
                 />
                 <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
                   d="M19.0021 12.784C19.0213 12.7502 19.0397 12.7163 19.0575 12.6823C19.0664 12.7002 19.0755 12.7179 19.0847 12.7355C19.0567 12.751 19.0291 12.7672 19.0021 12.784ZM8.37404 20.4447C9.20153 20.1729 9.81202 19.5446 10.0912 18.7861C10.9966 19.5148 12.5975 19.7073 14.2137 19.1764C14.6482 19.0336 15.0508 18.8495 15.413 18.6346C15.413 18.7067 15.4242 18.7799 15.4479 18.8521C15.5677 19.2167 15.9602 19.4151 16.3248 19.2954C16.6893 19.1756 16.8877 18.783 16.768 18.4185C16.6942 18.1939 16.5169 18.0324 16.3064 17.9695C16.4733 17.8134 16.623 17.65 16.754 17.4815C16.8779 17.322 17.1489 17.3521 17.2119 17.544L17.6087 18.7517C17.8681 19.5415 18.7187 19.9714 19.5085 19.712C20.2984 19.4525 20.7283 18.6019 20.4688 17.8121L19.9421 16.2086C20.1469 16.2132 20.3559 16.1836 20.5616 16.116C21.4003 15.8405 21.9093 15.0234 21.8281 14.18C22.0071 14.1588 22.1867 14.1196 22.3647 14.0612C23.8025 13.5888 24.5852 12.0403 24.1129 10.6024C23.6405 9.16457 22.092 8.38187 20.6541 8.85422C19.9263 9.09332 19.3663 9.60817 19.0521 10.2448C18.2676 8.99581 16.2454 8.51497 14.2206 9.18014C12.8226 9.63941 11.7495 10.5378 11.2551 11.5342C10.423 10.9684 9.34678 10.7812 8.31689 11.1196C6.55501 11.6984 5.59593 13.5958 6.17472 15.3577C6.18351 15.3845 6.19261 15.4111 6.202 15.4375C5.06478 16.0587 4.49634 17.4212 4.91531 18.6965C5.38766 20.1344 6.93618 20.9171 8.37404 20.4447Z"
                   fill="url(#paint5_linear_767_1512)"
                 />
                 <path
                   d="M1 12.75L6.95 6.80005"
                   stroke="#F4C139"
-                  stroke-width="1.7"
+                  strokeWidth="1.7"
                 />
                 <path
                   d="M13.75 1.73543L14.3554 2.81307L14.5467 3.1535L14.9295 3.23019L16.1415 3.47296L15.3037 4.38174L15.039 4.66883L15.0844 5.05665L15.228 6.28433L14.1048 5.76835L13.75 5.60535L13.3952 5.76835L12.272 6.28433L12.4156 5.05665L12.461 4.66883L12.1963 4.38174L11.3585 3.47296L12.5705 3.23019L12.9533 3.1535L13.1446 2.81307L13.75 1.73543Z"
                   fill="#FFFCDF"
                   stroke="#F4C139"
-                  stroke-width="1.7"
+                  strokeWidth="1.7"
                 />
                 <defs>
                   <linearGradient
@@ -131,8 +131,8 @@ const HomePage = () => {
                     y2="34.8478"
                     gradientUnits="userSpaceOnUse"
                   >
-                    <stop stop-color="#F5A92F" />
-                    <stop offset="1" stop-color="#F3D03E" />
+                    <stop stopColor="#F5A92F" />
+                    <stop offset="1" stopColor="#F3D03E" />
                   </linearGradient>
                   <linearGradient
                     id="paint1_linear_767_1512"
@@ -142,8 +142,8 @@ const HomePage = () => {
                     y2="29.7342"
                     gradientUnits="userSpaceOnUse"
                   >
-                    <stop stop-color="#F58C14" />
-                    <stop offset="1" stop-color="#F3BD1C" />
+                    <stop stopColor="#F58C14" />
+                    <stop offset="1" stopColor="#F3BD1C" />
                   </linearGradient>
                   <linearGradient
                     id="paint2_linear_767_1512"
@@ -153,8 +153,8 @@ const HomePage = () => {
                     y2="29.0113"
                     gradientUnits="userSpaceOnUse"
                   >
-                    <stop stop-color="#F58C14" />
-                    <stop offset="1" stop-color="#F3BD1C" />
+                    <stop stopColor="#F58C14" />
+                    <stop offset="1" stopColor="#F3BD1C" />
                   </linearGradient>
                   <linearGradient
                     id="paint3_linear_767_1512"
@@ -164,8 +164,8 @@ const HomePage = () => {
                     y2="27.5658"
                     gradientUnits="userSpaceOnUse"
                   >
-                    <stop stop-color="#F58C14" />
-                    <stop offset="1" stop-color="#F3BD1C" />
+                    <stop stopColor="#F58C14" />
+                    <stop offset="1" stopColor="#F3BD1C" />
                   </linearGradient>
                   <linearGradient
                     id="paint4_linear_767_1512"
@@ -175,8 +175,8 @@ const HomePage = () => {
                     y2="28.2886"
                     gradientUnits="userSpaceOnUse"
                   >
-                    <stop stop-color="#F58C14" />
-                    <stop offset="1" stop-color="#F3BD1C" />
+                    <stop stopColor="#F58C14" />
+                    <stop offset="1" stopColor="#F3BD1C" />
                   </linearGradient>
                   <linearGradient
                     id="paint5_linear_767_1512"
@@ -186,8 +186,8 @@ const HomePage = () => {
                     y2="20.6998"
                     gradientUnits="userSpaceOnUse"
                   >
-                    <stop stop-color="#ECECEC" />
-                    <stop offset="1" stop-color="white" />
+                    <stop stopColor="#ECECEC" />
+                    <stop offset="1" stopColor="white" />
                   </linearGradient>
                 </defs>
               </svg>
